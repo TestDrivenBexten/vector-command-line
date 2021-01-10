5a07b94bb66c776a45c033a9a28c7da2db467eb2
Skyler Bexten <skyler.bexten@gmail.com>
2021-01-09 19:08:11 -0700

    Pass first test

--- src/main/kotlin/myvector/VectorMath.kt
+++ src/main/kotlin/myvector/VectorMath.kt
@@ -3,5 +3,5 @@ package myvector
 data class KotlinVector(val a: Int, val b: Int)
 
 fun combineVectors(vector1: KotlinVector, vector2: KotlinVector): KotlinVector {
-    return KotlinVector(0,0)
+    return KotlinVector(vector1.a + vector2.a,vector1.b + vector2.b)
 }
